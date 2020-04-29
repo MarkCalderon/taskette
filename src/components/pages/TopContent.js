b1a1a8ce39f5cffe5906ecbbd6452bac2d536a3f
Mark Calderon <mark_calderon@commude.ph>
2020-04-29 16:59:40 +0800

    [edit] added changes to styling

--- src/components/pages/TopContent.js
+++ src/components/pages/TopContent.js
@@ -7,17 +7,23 @@ class TopContent extends React.Component {
 
     constructor() {
         super()
+
+        // Initialize the state,
+        // State is an object {},
+        // PostList is an array []
         this.state = {
             postList: []
         }
     }
 
+    // Fetch Data from JSONPlaceholder
     fetchData() {
         fetch('https://jsonplaceholder.typicode.com/posts/')
             .then((response) => {
                 return response.json()
             })
             .then((data) => {
+                // Stores setState to object without function.
                 this.setState({
                     postList: data
                 })
@@ -29,12 +35,17 @@ class TopContent extends React.Component {
     }
 
     render() {
-
+        // Slices/take only 5 items out of the original Array
+        // Map only works for Arrays
         const dataIn = this.state.postList.slice(0,5).map(item => {
+            // postList is passed as an item.
             return (
+                    // Pass values of the state to the component as a prop.
                     <ListData key={item.id} title={item.title} description={item.body} />
             )
         })
+
+
         return (
             <main>
                 <div className="inner">
